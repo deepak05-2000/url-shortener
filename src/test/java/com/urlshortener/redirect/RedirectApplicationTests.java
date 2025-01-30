9da3a4ca52cab419e77b0bf34ffb591ec9997781
Deepak Bijalwan <69871514+deepak05-2000@users.noreply.github.com>
2025-01-31 02:57:43 +0530

    Initial commit

---
+++ src/test/java/com/urlshortener/redirect/RedirectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.urlshortener.redirect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedirectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
